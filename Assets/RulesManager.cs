1d4e9d5b8867897a8e762b0741089d426941aecd
CERULEANGAMEDEV\alex <bacco.a10@gmail.com>
2025-02-16 23:06:10 +0100

    rules logic ok - a1 wip

--- Assets/RulesManager.cs
+++ Assets/RulesManager.cs
@@ -1,5 +1,7 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,22 +17,9 @@ public class RulesManager : MonoBehaviour
 
     [SerializeField] Sprite[] languageIcons;
 
-    Dictionary<string, string> levelAndRules = new Dictionary<string, string>(); 
     Dictionary<string, Sprite> iconsAndLanguages = new Dictionary<string, Sprite>(); 
     
-    public string rules_title_placeHolder = "Titleeeeeeeeeeeeeeeeeee";
-    public string rules_body_placeHolder = "bodyyyyyyyyyyyyyyyyy";
-    private void InitializeLevelAndRulesDictionary()
-    {
-        levelAndRules.Add("A1", rules_title_placeHolder + "\n" + rules_body_placeHolder);
-        levelAndRules.Add("A2", "A2 Rules");
-        levelAndRules.Add("B1", "B1 Rules");
-        levelAndRules.Add("B2", "B2 Rules");
-        levelAndRules.Add("C1", "C1 Rules");
-        levelAndRules.Add("C2", "C2 Rules");
 
-        Debug.Log("Levels And Rules dict initialized");
-    }
     private void InitializeIconsAndLanguagesDictionary()
     {
         iconsAndLanguages.Add("france", languageIcons[0]);
@@ -47,11 +36,21 @@ public class RulesManager : MonoBehaviour
 
     private void Start()
     {
-        //InitDutchA1Rule();
         if ("6 - Difficulty Rules".Equals(SceneManager.GetActiveScene().name)) {
-            //InitializeLevelAndRulesDictionary();
-            InitDutchA1Rule();
+            // InitDutchA1Rule();
             InitializeIconsAndLanguagesDictionary();
+
+            switch (GameManager.Instance.selectedLanguage)
+            {
+                case "Dutch":
+                    switch (GameManager.Instance.selectedDifficulty)
+                    {
+                        case "A1": InitDutchA1Rule(); UpdateTextA1_withPages(page_counter); break;
+                        default: throw new Exception("Error On selectedDifficulty: ");
+                    }
+                    break;
+                default: throw new Exception("Error On selectedLanguage: ");
+            }
         }
         StartCoroutine(UpdateDifficultyRules());
         StartCoroutine(UpdateLanguageIcon());
@@ -60,19 +59,6 @@ public class RulesManager : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
         difficultyLevel.text = GameManager.Instance.selectedDifficulty;
-        //Debug.Log("difficultyLevel " + difficultyLevel.text);
-        //if (textRules != null) {
-        //    switch (difficultyLevel.text)
-        //    {
-        //        case "A1": textRules.text = levelAndRules["A1"]; break;
-        //        case "A2": textRules.text = levelAndRules["A2"]; break;
-        //        case "B1": textRules.text = levelAndRules["B1"]; break;
-        //        case "B2": textRules.text = levelAndRules["B2"]; break;
-        //        case "C1": textRules.text = levelAndRules["C1"]; break;
-        //        case "C2": textRules.text = levelAndRules["C2"]; break;
-        //        default: Debug.Log("Error"); break;
-        //    }
-        //}
     }
 
     public IEnumerator UpdateLanguageIcon()
@@ -96,30 +82,49 @@ public class RulesManager : MonoBehaviour
 
     public TMP_Text rules_title_txt;
     public TMP_Text rules_body_txt;
+    Dictionary<string, string> dutch_a1_rules_titles_and_bodies = new Dictionary<string, string>();
+    Dictionary<string, string> dutch_a2_rules_titles_and_bodies = new Dictionary<string, string>(); // non usato ancora atm
+    Dictionary<string, Dictionary<string, string>> dutch_levels_and_rules = new Dictionary<string, Dictionary<string, string>>();
+    public int page_counter = 0; // 10 pagine in totale, quindi da 0 a 9
     public void InitDutchA1Rule() { // sia all'inizio che quando cambi pagina (?) forse quando cambi pagina puoi ottimizzare oppure metti i tag sulle pagine e li filli cosi
 
-        /*
-            Dutch
-                A1
-                    InitializeDutchA1Dictionary
-                        <A1, dictionary<rulesTitle, RulesBody>
-         */
-
-        Dictionary<string, string> dutch_a1_rules_titles_and_bodies = new Dictionary<string, string>();
-        dutch_a1_rules_titles_and_bodies.Add("Rules 1", "Body 1");
-        Dictionary<string, Dictionary<string, string>> dutch_levels_and_rules = new Dictionary<string, Dictionary<string, string>>();
-        dutch_levels_and_rules.Add("A1", dutch_a1_rules_titles_and_bodies);
-
-        foreach (var coppia_frasi in dutch_levels_and_rules.Values)
+        dutch_a1_rules_titles_and_bodies.Add("Pronunciation and Special Sounds", "The \"g\" sounds like a guttural \"h\" (e.g., goed → /ɣut/).\r\n\nThe \"ui\" sounds similar to the French \"œy\" (e.g., huis → /hœys/).\r\n\nThe \"ij\" and \"ei\" are pronounced like a mix between \"ai\" and \"ei\".\r\n\nThe \"sch\" sounds like an \"s\" followed by a guttural \"ch\" (e.g., school → /sxoːl/).");
+        dutch_a1_rules_titles_and_bodies.Add("Definite and Indefinite Articles", "De → Used for most common nouns (de man = the man, de vrouw = the woman).\r\nHet → Used for neutral words and diminutives (het huis = the house, het kind = the child).\r\nEen → Indefinite article, meaning \"a\" or \"an\" (een boek = a book).");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 3", "A1 Body 3");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 4", "A1 Body 4");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 5", "A1 Body 5");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 6", "A1 Body 6");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 7", "A1 Body 7");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 8", "A1 Body 8");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 9", "A1 Body 9");
+        dutch_a1_rules_titles_and_bodies.Add("A1 Rules 10", "A1 Body 10");
+    }
+    public void UpdateTextA1_withPages(int counter)
+    {
+        // Supponiamo che `counter` sia già definito e rappresenti l'indice attuale
+        if (counter >= 0 && counter < dutch_a1_rules_titles_and_bodies.Count)
         {
-            Debug.Log("------------");
-            foreach (var singola_frase in coppia_frasi)
-            {
-                Debug.Log("A porco ziiiiiiiiiiiii Sono il titolo della regola: " + singola_frase.Key);
-                Debug.Log("A porco ziiiiiiiiiiiii Sono il body della regola: " + singola_frase.Value);
-                rules_title_txt.text = singola_frase.Key;
-                rules_body_txt.text = singola_frase.Value;
-            }
+            var elemento = dutch_a1_rules_titles_and_bodies.ElementAt(counter); // Prende l'elemento alla posizione 'counter'
+
+            rules_title_txt.text = elemento.Key;   // Chiave
+            rules_body_txt.text = elemento.Value;  // Valore
+        }
+
+    }
+    public void SwitchPageForward()
+    {
+        page_counter++;
+        if(page_counter <= 9)
+        {
+            UpdateTextA1_withPages(page_counter);
+        }
+    }
+    public void SwitchPageBackward()
+    {
+        page_counter--;
+        if (page_counter >= 0)
+        {
+            UpdateTextA1_withPages(page_counter);
         }
     }
 }
