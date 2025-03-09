c88697bc66b9c7582c71f6af4b9a88a9952d9271
CERULEANGAMEDEV\alex <bacco.a10@gmail.com>
2025-03-09 12:55:43 +0100

    refactoring rules and phrases

--- Assets/EnglishLogic.cs
+++ Assets/EnglishLogic.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -18,14 +17,51 @@ public class EnglishLogic : MonoBehaviour
     public TMP_Text rule_dynamic_text;
     public string selectedTense = ""; // Cambia a seconda dell'esercizio
 
+    //private Dictionary<string, List<string>> questionTemplates = new Dictionary<string, List<string>>()
+    //{
+    //    { "Present Simple", new List<string> { "Do {subject} {verb} {object}?", "Does {subject} {verb} {object}?" } },
+    //    { "Past Simple", new List<string> { "Did {subject} {verb} {object}?" } },
+    //    { "Present Continuous", new List<string> { "Is {subject} {verb-ing} {object}?", "Are {subject} {verb-ing} {object}?" } },
+    //    { "Past Continuous", new List<string> { "Was {subject} {verb-ing} {object}?", "Were {subject} {verb-ing} {object}?" } },
+    //    { "Future Simple", new List<string> { "Will {subject} {verb} {object}?" } },
+    //    { "Present Perfect", new List<string> { "Have {subject} {verb-past} {object}?", "Has {subject} {verb-past} {object}?" } }
+    //};
     private Dictionary<string, List<string>> questionTemplates = new Dictionary<string, List<string>>()
     {
-        { "Present Simple", new List<string> { "Do {subject} {verb} {object}?", "Does {subject} {verb} {object}?" } },
-        { "Past Simple", new List<string> { "Did {subject} {verb} {object}?" } },
-        { "Present Continuous", new List<string> { "Is {subject} {verb-ing} {object}?", "Are {subject} {verb-ing} {object}?" } },
-        { "Past Continuous", new List<string> { "Was {subject} {verb-ing} {object}?", "Were {subject} {verb-ing} {object}?" } },
-        { "Future Simple", new List<string> { "Will {subject} {verb} {object}?" } },
-        { "Present Perfect", new List<string> { "Have {subject} {verb-past} {object}?", "Has {subject} {verb-past} {object}?" } }
+        { "Present Simple", new List<string>
+            {
+                "Do {subject} {verb} {object} {preposition} {complement} {adverb}?",
+                "Does {subject} {verb} {object} {preposition} {complement} {adverb}?"
+            }
+        },
+        { "Past Simple", new List<string>
+            {
+                "Did {subject} {verb} {object} {preposition} {complement} {adverb}?"
+            }
+        },
+        { "Present Continuous", new List<string>
+            {
+                "Is {subject} {verb-ing} {object} {preposition} {complement} {adverb}?",
+                "Are {subject} {verb-ing} {object} {preposition} {complement} {adverb}?"
+            }
+        },
+        { "Past Continuous", new List<string>
+            {
+                "Was {subject} {verb-ing} {object} {preposition} {complement} {adverb}?",
+                "Were {subject} {verb-ing} {object} {preposition} {complement} {adverb}?"
+            }
+        },
+        { "Future Simple", new List<string>
+            {
+                "Will {subject} {verb} {object} {preposition} {complement} {adverb}?"
+            }
+        },
+        { "Present Perfect", new List<string>
+            {
+                "Have {subject} {verb-past} {object} {preposition} {complement} {adverb}?",
+                "Has {subject} {verb-past} {object} {preposition} {complement} {adverb}?"
+            }
+        }
     };
     private Dictionary<string, string> affirmativeTemplate = new Dictionary<string, string>()
     {
@@ -47,8 +83,8 @@ public class EnglishLogic : MonoBehaviour
     };
 
     // piuttosto che creare un dizionario infinito, mostra la lista di parole disponibili magari
-    private HashSet<string> singularPersons = new HashSet<string>() { "he", "she", "it" };
-    private HashSet<string> pluralPersons = new HashSet<string>() { "i","you", "we", "they" };
+    private HashSet<string> singularSubjects = new HashSet<string>() { "he", "she", "it" };
+    private HashSet<string> pluralSubjects = new HashSet<string>() { "i","you", "we", "they" };
 
     private HashSet<string> singleVerbsforQuestion = new HashSet<string>() { "eat", "like", "play", "read", "write", "run", "speak", "swim" };
     private HashSet<string> singleVerbsforAffirmationsAndNegations = new HashSet<string>() { "eats", "likes", "plays", "reads", "writes", "runs", "speaks", "swims" };
@@ -56,7 +92,42 @@ public class EnglishLogic : MonoBehaviour
     private HashSet<string> pastVerbs = new HashSet<string>() { "ate", "liked", "played", "read", "wrote", "ran", "spoke", "swam" };
     private HashSet<string> futureVerbs = new HashSet<string>() { "eat", "like", "play", "read", "write", "run", "speak", "swim" };
 
-    private HashSet<string> nouns = new HashSet<string>() { "apples", "football", "books", "letters", "games", "music", "movies", "languages" };
+    private HashSet<string> objects = new HashSet<string>() { "apples", "football", "books", "letters", "games", "music", "movies", "languages" };
+
+    private HashSet<string> prepositions = new HashSet<string>
+    {
+        "aboard", "about", "above", "across", "after", "against", "along", "amid", "among", "anti", "around", "as", "at", "before", "behind", "below", "beneath", "beside", 
+        "besides", "between", "beyond", "but", "by", "concerning", "considering", "despite", "down", "during", "except", "excepting", "excluding", "following", "for", "from", "in", 
+        "inside", "into", "like", "minus", "near", "of", "off", "on", "onto", "opposite", "outside", "over", "past", "per", "plus", "regarding", "round", "save", "since", "than", 
+        "through", "to", "toward", "towards", "under", "underneath", "unlike", "until", "up", "upon", "versus", "via", "with", "within", "without", "in the"
+    };
+
+    private HashSet<string> adverbs = new HashSet<string>
+        {
+            "abnormally", "accidentally", "acidly", "actually", "adventurously", "afterwards", "almost", "always", "angrily", "annually", "anxiously", "arrogantly", "awkwardly", "badly", 
+            "bashfully", "beautifully", "bitterly", "bleakly", "blindly", "blissfully", "boastfully", "boldly", "bravely", "briefly", "brightly", "briskly", "broadly", "busily", "calmly", 
+            "carefully", "carelessly", "cautiously", "certainly", "cheerfully", "clearly", "cleverly", "closely", "coaxingly", "commonly", "continually", "coolly", "correctly", 
+            "courageously", "crossly", "cruelly", "curiously", "daily", "daintily", "dearly", "deceivingly", "deeply", "defiantly", "deliberately", "delightfully", "diligently", "dimly", 
+            "doubtfully", "dreamily", "easily", "elegantly", "energetically", "enormously", "enthusiastically", "equally", "especially", "evenly", "eventually", "exactly", "excitedly", 
+            "extremely", "fairly", "faithfully", "famously", "fatally", "ferociously", "fervently", "fiercely", "fondly", "foolishly", "fortunately", "frankly", "frantically", "freely", 
+            "frenetically", "frightfully", "fully", "furiously", "generally", "generously", "gently", "gladly", "gleefully", "gracefully", "gratefully", "greatly", "greedily", "happily", 
+            "hastily", "healthily", "heavily", "helpfully", "helplessly", "highly", "honestly", "hopelessly", "hungrily", "immediately", "innocently", "inquisitively", "instantly", 
+            "intensely", "intently", "interestingly", "inwardly", "irritably", "jaggedly", "jealously", "jovially", "joyfully", "joyously", "jubilantly", "judgementally", "justly", 
+            "keenly", "kiddingly", "kindheartedly", "kindly", "knavishly", "knowingly", "knowledgeably", "lazily", "lightly", "likely", "limply", "lively", "loftily", "longingly", 
+            "loosely", "loudly", "lovingly", "loyally", "madly", "majestically", "meaningfully", "mechanically", "merrily", "miserably", "mockingly", "monthly", "mortally", "mostly", 
+            "mysteriously", "naturally", "nearly", "neatly", "needily", "nervously", "never", "nicely", "noisily", "now", "nowhere", "obediently", "obnoxiously", "oddly", "offensively", 
+            "officially", "often", "only", "openly", "optimistically", "overconfidently", "owlishly", "painfully", "partially", "patiently", "perfectly", "physically", "playfully", 
+            "politely", "poorly", "positively", "potentially", "powerfully", "promptly", "properly", "punctually", "quaintly", "quarrelsomely", "queasily", "queerly", "questionably", 
+            "questioningly", "quicker", "quickly", "quietly", "quirkily", "quizzically", "rapidly", "rarely", "readily", "really", "reassuringly", "recklessly", "regularly", "reluctantly", 
+            "repeatedly", "reproachfully", "restfully", "righteously", "rightfully", "rigidly", "roughly", "rudely", "sadly", "safely", "scarcely", "scarily", "searchingly", "sedately", 
+            "seemingly", "seldom", "selfishly", "separately", "seriously", "shakily", "sharply", "sheepishly", "shrilly", "shyly", "silently", "sleepily", "slowly", "smoothly", "softly", 
+            "solemnly", "solidly", "sometimes", "soon", "speedily", "stealthily", "sternly", "strictly", "successfully", "suddenly", "surprisingly", "suspiciously", "sweetly", "swiftly", 
+            "sympathetically", "tenderly", "tensely", "terribly", "thankfully", "there", "thoroughly", "thoughtfully", "tightly", "tomorrow", "too", "tremendously", "triumphantly", "truly",
+            "truthfully", "ultimately", "unabashedly", "unaccountably", "unbearably", "unethically", "unexpectedly", "unfortunately", "unimpressively", "unnaturally", "unnecessarily", 
+            "upliftingly", "upwardly", "urgently", "usefully", "uselessly", "usually", "utterly", "vacantly", "vaguely", "vainly", "valiantly", "vastly", "verbally", "very", "viciously", 
+            "victoriously", "violently", "vivaciously", "voluntarily", "warmly", "weakly", "wearily", "wetly", "wholly", "wildly", "willfully", "wilfully", "wisely", "woefully", 
+            "wonderfully", "worriedly", "wrongly", "yawningly", "yearly", "yearningly", "yesterday", "yieldingly", "youthfully", "zealously", "zestfully", "zestily"
+        };
 
     void Start()
     {
@@ -90,6 +161,7 @@ public class EnglishLogic : MonoBehaviour
             feedbackText.text = "Tense not recognized.";
             return;
         }
+
         switch (phraseTypeDropdown.options[phraseTypeDropdown.value].text)
         {
             case "Questions":
@@ -127,12 +199,30 @@ public class EnglishLogic : MonoBehaviour
         switch (phraseTypeDropdown.options[phraseTypeDropdown.value].text)
         {
             case "Questions":
-                // question logic
+                // question logic - COMPLEMENT IS AN OBJECT
+                // top-down in or - re organize it
+                /*
+                 {subject} {adverb} {verb} {object} {preposition} {object} {preposition} {place} {preposition} {time}
+                 {subject} {adverb} {verb} {object} {preposition} {place} {preposition} {time}.
+                 {subject} {verb} {object} {preposition} {place} {preposition} {time}. etc...
+                 {subject} {verb} {object} {preposition} {place}.
+                 {subject} {verb} {object}.
+                 {subject} {verb}.
+                
+                S + V → "She runs."
+                S + V + O → "She reads books."
+                S + V + O + Prep + Loc → "She reads books in the library."
+                S + V + O + Prep + Loc + Prep + Temp → "She reads books in the library after school."
+                S + Adv + V + O + Prep + Loc + Prep + Temp → "She quickly reads books in the library after school."
+                S + Adv + V + O + Prep + O + Prep + Loc + Prep + Temp → "She quickly reads books about science in the library after school."
+                S + Adv + V + O + Prep + O + Prep + O + Prep + Loc + Prep + Temp + Adv → "She quickly reads books about science with her friends in the library after school happily."
+                 */
                 if (input.Contains("?"))
                 {
                     if (inputWords[0] == "do")
                     {
-                        if (pluralPersons.Contains(inputWords[1]) && singleVerbsforQuestion.Contains(inputWords[2]) && nouns.Contains(inputWords[3]))
+                        if (pluralSubjects.Contains(inputWords[1]) && ( singleVerbsforQuestion.Contains(inputWords[2]) || continuousVerbs.Contains(inputWords[2])) && objects.Contains(inputWords[3]) &&
+                            prepositions.Contains(inputWords[4]) && adverbs.Contains(inputWords[5]))
                         {
                             Debug.Log("The phrase is grammatically correct.");
                             return true;
@@ -140,7 +230,7 @@ public class EnglishLogic : MonoBehaviour
                     }
                     else if (inputWords[0] == "does")
                     {
-                        if (singularPersons.Contains(inputWords[1]) && singleVerbsforQuestion.Contains(inputWords[2]) && nouns.Contains(inputWords[3]))
+                        if (singularSubjects.Contains(inputWords[1]) && singleVerbsforQuestion.Contains(inputWords[2]) && objects.Contains(inputWords[3]))
                         {
                             Debug.Log("The phrase is grammatically correct.");
                             return true;
